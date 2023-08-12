baeb12b2aac571c00365a2caf9cdb8c2e4fdd65d
codingwithgit <141912182+codingwithgit@users.noreply.github.com>
2023-08-12 13:17:58 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -28,3 +28,49 @@ public class RewardValueTests {
         assert false;
     }
 }
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RewardValueTests {
+	
+	
+	
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        double milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+    	double cashValue = 100;
+        double expectedMilesValue = cashValue / 0.0035; 
+        var rewardValue = new RewardValue(cashValue);
+        double actualMilesValue = rewardValue.getMilesValue();
+        assertEquals(expectedMilesValue, actualMilesValue);
+       // assert false;
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+    	    double milesValue = 10000;
+    	    double expectedCashValue = milesValue * 0.0035; 
+    	    var rewardValue = new RewardValue(expectedCashValue,milesValue);
+    	    double actualCashValue = rewardValue.getCashValue();
+    	    assertEquals(expectedCashValue, actualCashValue);
+      //  assert false;
+    }
+}
